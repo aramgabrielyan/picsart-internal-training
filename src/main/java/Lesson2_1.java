44c1e6cfbd2ee43ea405137ea6b482527ae0590a
aramgabrielyan <gabrielyan.sm@gmail.com>
2021-05-12 14:33:55 +0400

    Lesson 2 Homework

--- src/main/java/Lesson2_1.java
+++ src/main/java/Lesson2_1.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Lesson2_1 {
+
+    static final String TEXT_PASSWORD = "password";
+    static final String TEXT_USERNAME = "username";
+
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "src/chromedriver");
         WebDriver driver = new ChromeDriver();
@@ -13,12 +17,12 @@ public class Lesson2_1 {
         WebElement loginButton = driver.findElement(By.cssSelector("[data-test='headerAuth-signInBtn pa-uiLib-headerAuth-authBtn']"));
         loginButton.click();
         new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfAllElementsLocatedBy((By.name("username"))));
-        WebElement usernameField = driver.findElement(By.name("username"));
+        WebElement usernameField = driver.findElement(By.name(TEXT_USERNAME));
         usernameField.sendKeys("smart_offer");
         WebElement signInButton = driver.findElement(By.cssSelector("[class*='pa-uiLib-authentication-btn primary']"));
         boolean isDisabled = signInButton.getAttribute("class").contains("pa-uiLib-authentication-button-disabled");
         if (isDisabled) {
-            WebElement passwordField = driver.findElement(By.name("password"));
+            WebElement passwordField = driver.findElement(By.name(TEXT_PASSWORD));
             passwordField.sendKeys("Lusin86");
             signInButton.click();
         }
