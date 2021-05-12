7315f34592173c451dc19c3c050927a34c49b5b9
aramgabrielyan <gabrielyan.sm@gmail.com>
2021-05-12 20:11:58 +0400

    Lesson 2 Homework

--- src/main/java/Lesson2_1.java
+++ src/main/java/Lesson2_1.java
@@ -7,9 +7,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Lesson2_1 {
 
-    static final String TEXT_PASSWORD = "password";
-    static final String TEXT_USERNAME = "username";
-
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "src/chromedriver");
         WebDriver driver = new ChromeDriver();
@@ -17,12 +14,12 @@ public class Lesson2_1 {
         WebElement loginButton = driver.findElement(By.cssSelector("[data-test='headerAuth-signInBtn pa-uiLib-headerAuth-authBtn']"));
         loginButton.click();
         new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfAllElementsLocatedBy((By.name("username"))));
-        WebElement usernameField = driver.findElement(By.name(TEXT_USERNAME));
+        WebElement usernameField = driver.findElement(By.name("username"));
         usernameField.sendKeys("smart_offer");
         WebElement signInButton = driver.findElement(By.cssSelector("[class*='pa-uiLib-authentication-btn primary']"));
         boolean isDisabled = signInButton.getAttribute("class").contains("pa-uiLib-authentication-button-disabled");
         if (isDisabled) {
-            WebElement passwordField = driver.findElement(By.name(TEXT_PASSWORD));
+            WebElement passwordField = driver.findElement(By.name("password"));
             passwordField.sendKeys("Lusin86");
             signInButton.click();
         }
---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+    private  WebDriver driver;
+
+    private By usernameFieldLocation = By.name("username");
+    private By passwordFieldLocation = By.name("password");
+    private By loginButtonLocation = By.cssSelector("[data-test='headerAuth-signInBtn pa-uiLib-headerAuth-authBtn']");
+    private By signInButtonLocation = By.cssSelector("[class*='pa-uiLib-authentication-btn primary']");
+    private By avatarLocation = By.className("pa-uiLib-headerProfileInfo-avatarBlock");
+
+    public LoginPage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickLoginButton(){
+        WebElement loginButton = driver.findElement(loginButtonLocation);
+        loginButton.click();
+        new WebDriverWait(driver, 2).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(usernameFieldLocation));
+
+    }
+
+    public void typeUsername(String username){
+        WebElement usernameField = driver.findElement(usernameFieldLocation);
+        usernameField.sendKeys(username);
+    }
+
+    public void typePassword(String password){
+        WebElement passwordField = driver.findElement(passwordFieldLocation);
+        passwordField.sendKeys(password);
+    }
+
+    public void clickSignInButton(){
+        WebElement signInButton = driver.findElement(signInButtonLocation);
+        signInButton.click();
+    }
+
+    public boolean isAvatarDisplayed(){
+        return driver.findElement(avatarLocation).isDisplayed();
+    }
+
+    public boolean isUserLoggedIn(){
+        WebElement avatarIcon = new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(avatarLocation));
+        return avatarIcon.isDisplayed();
+    }
+
+
+
+
+
+
+}
